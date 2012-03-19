abf039e7c19309a64466edbf733fa62d7df6caf1
juliefab <juliefab2009@gmail.com>
2012-03-19 13:41:22 -0500

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyjobz::Application.config.secret_token = '35a25cb0c8ba2c7457572ad25a56811e614d73993fa31ab7a095450e19ca8975031e35c8470d498977d19a1e6fc6f65082b33fcec6f87bb78fe52b97de3013b5'
